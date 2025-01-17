09861578f2bc71e0e26e4527139474de35c21981
Guru-Devops-creator <gurut591@gmail.com>
2025-01-17 10:42:21 +0530

    testing

---
+++ datasources/ec2.tf
@@ -0,0 +1,37 @@
+resource "aws_instance" "terraform-guru" {
+  ami                    = data.aws_ami.devops.id 
+  instance_type          = "t3.large"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_terraform-guru.id]
+  tags = {
+    Name = "terraform"
+  }
+
+}
+resource "aws_security_group" "allow_ssh_terraform-guru" {
+  name        = "allow_ssh"
+  description = "allow port number 22 for ssh connections"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    name = "allow_sshh"
+
+
+  }
+
+}
---
+++ locals/varaiables.tf
@@ -0,0 +1,20 @@
+variable "instance_names" {
+  type    = list(string)
+  default = ["mysql", "frontend", "backend"]
+
+}
+
+# variable "domain_name" {
+#   default = "aws-devops81s.online"
+# }
+
+# variable "zone_id" {
+#   type    = string
+#   default = "Z09029931RLS4GGI08VA6" 
+
+# }
+
+variable "environment" {
+  default     = "prod"
+  description = "description"
+}
